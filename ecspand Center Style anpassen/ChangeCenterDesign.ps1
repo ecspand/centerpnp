efefe7605ac362355a6cb6b49b0edfd5bea41171
Titz Peter <peter.titz@d-velop.de>
2016-05-31 09:56:24 +0200

    ecspand Center Style anpassen

---
+++ ecspand Center Style anpassen/ChangeCenterDesign.ps1
@@ -0,0 +1,67 @@
+#
+###############################################################################################################################
+# Bitte ausfüllen
+#
+# Geben Sie hier die neuen Farbwerte an. Wichtig achten Sie auf die Darstellung Hex und RGB
+# Die Beiden CSS Dateien Fileview und Kendoo werden zusammengefasst um diese Später in der Inhaltskonfiguration des Centers nachzuladen
+$Color1New = "005da8" # Hex
+$Color2New = "0,93,168" # RGB
+$Color3New = "156,206,240" # RGB
+$Color4New = "153,199,232" # RGB
+
+# Geben Sie hier den Pfad der beiden ecspand Center CSS - Dateien an
+$CssDateiPfadFielView = "C:\ecspandDesign\Styles\FileView.min.css"
+$CssDateiPfadKendo = "C:\ecspandDesign\Styles\kendo.ecspand.min.css"
+
+
+
+
+################################################################################################################################
+################################################################################################################################
+# Nicht verändern
+#
+# Initialisierung der Farben
+$Color1 = "0072c6"
+$Color2 = "0,114,198" 
+$Color3 = "156,206,240"
+$Color4 = "153,199,232"
+
+
+function Change-CenterDesign{
+    param(
+        [String]$CssPathFileView,
+        [String]$CssPathKendoo
+    )
+    
+    #$NewFileName = $CssPath.Substring($CssPath.LastIndexOf("\")+1);
+    #$NewFileName = "New_$NewFileName";
+    $NewFileName = "CustomCenter.css"
+    $NewFilePath = $CssPathFileView.Substring(0, $CssPathFileView.LastIndexOf("\")+1);
+    $NewFilePath = $NewFilePath + $NewFileName
+
+    [String]$CssFile = Get-Content $CssPathFileView;
+    [String]$CssFile2 = Get-Content $CssPathKendoo;
+
+    # Fileview
+    
+    $CssFileNew = $CssFile.Replace($color1, $Color1New);
+    $CssFileNew = $CssFileNew.Replace($color2, $Color2New);
+    $CssFileNew = $CssFileNew.Replace($color3, $Color3New);
+    $CssFileNew = $CssFileNew.Replace($color4, $Color4New);
+
+    # Kendoo
+    $CssFileNew2 = $CssFile2.Replace($color1, $Color1New) 
+
+
+    $CssFileNew = $CssFileNew, "" ,$CssFileNew2
+    write-host "$newFilePath"
+    write-host "Schreibe Datei $NewFileName"
+
+    $CssFileNew | Out-File $NewFilePath;
+
+}
+
+
+Change-CenterDesign -CssPathFileView $CssDateiPfadFielView -CssPathKendoo $CssDateiPfadKendo
+
+
