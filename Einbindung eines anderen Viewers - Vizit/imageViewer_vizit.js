c5d6bd66869e339f7a2de2e006501514a0882132
Michael Pawlik <michael.pawlik@d-velop.com>
2016-07-26 12:04:01 +0200

    Vizit Beispiel um Weiche für den Fall, dass ein Archiviertes DOkument angezeigt wird, ergänzt

--- Einbindung eines anderen Viewers - Vizit/imageViewer_vizit.js
+++ Einbindung eines anderen Viewers - Vizit/imageViewer_vizit.js
@@ -19,29 +19,44 @@ var Template_imageViewer_vizit = (function (_super) {
             // Url der anzuzeigenden Datei ermitteln
             this.ctx.getServerRelativeFileUrl().then(function(serverRelativeUrl) {
                 
-                try {
-                    // Optionen beinhalten u.a. die Information darüber wo drin der Viewer gerendert werden soll, hier wird der für die Viewer vorgesehene container ermittelt und das HTMLElement übergeben
-                    var vizitOptions = { renderTo: _this.container.find(".dvContainer")[0], autoShow: true },
-                        // Für die Ermittlung des Elementes benötigt der Vizit Viewer lediglich die absolute Url der Datei
-                        itemObject = { url: ecspand.Core.getServerUrl() + serverRelativeUrl };
+                // Falls es sich um ein Link-archiviertes Dokument handelt durchläuft es weiterhin die Standard-Routine
+                if (serverRelativeUrl.indexOf(".ecs") === -1) {
+                    try {
+                        // Optionen beinhalten u.a. die Information darüber wo drin der Viewer gerendert werden soll, hier wird der für die Viewer vorgesehene container ermittelt und das HTMLElement übergeben
+                        var vizitOptions = { renderTo: _this.container.find(".dvContainer")[0], autoShow: true },
+                            // Für die Ermittlung des Elementes benötigt der Vizit Viewer lediglich die absolute Url der Datei
+                            itemObject = { url: ecspand.Core.getServerUrl() + serverRelativeUrl };
+                            
+                        // Viewer Erstellen (weitere Informationen findet man unter http://files.vizit.com/public/docs/4.0/symbols/Vizit.Essential.Manager.html)
+                        Vizit.Essential.Manager.Create("centerVizit", vizitOptions);
+                        // Setzen des Items
+                        Vizit.Essential.Manager.Open("centerVizit", itemObject);
                         
-                    // Viewer Erstellen (weitere Informationen findet man unter http://files.vizit.com/public/docs/4.0/symbols/Vizit.Essential.Manager.html)
-                    Vizit.Essential.Manager.Create("centerVizit", vizitOptions);
-                    // Setzen des Items
-                    Vizit.Essential.Manager.Open("centerVizit", itemObject);
+                        // Der Visit Viewer wird standardmäßig mit einem fürs center zu geringem z-index dargestellt
+                        $(".x-panel").css({ zIndex: 10});
                     
-                    // Der Visit Viewer wird standardmäßig mit einem fürs center zu geringem z-index dargestellt
-                    $(".x-panel").css({ zIndex: 10});
-                
-                    dfd.resolve();
+                        dfd.resolve();
+                    }
+                    catch (err) {
+                        // Tritt beim Laden des Viewers ein Fehler auf, so können Ausführliche Informationen im Browser-Log oder als Notification ausgegeben werden
+                        dfd.reject(ecspand.Helper.Exception.createAndLogException({
+                            instance: this, method: "init", 
+                            notificationMessage: "Vizit Viewer konnte nicht geladen werden. Bitte stellen Sie sicher, dass alle Komponenten installiert wurden und eine gültige Lizenz vorhanden ist.<br/> Weiter Informationen finden Sie im Browser-Log.",
+                            message: "Vizit Viewer konnte nicht geladen werden", error: err
+                        }, false, true));
+                    }
                 }
-                catch (err) {
-                    // Tritt beim Laden des Viewers ein Fehler auf, so können Ausführliche Informationen im Browser-Log oder als Notification ausgegeben werden
-                    dfd.reject(ecspand.Helper.Exception.createAndLogException({
-                        instance: this, method: "init", 
-                        notificationMessage: "Vizit Viewer konnte nicht geladen werden. Bitte stellen Sie sicher, dass alle Komponenten installiert wurden und eine gültige Lizenz vorhanden ist.<br/> Weiter Informationen finden Sie im Browser-Log.",
-                        message: "Vizit Viewer konnte nicht geladen werden", error: err
-                    }, false, true));
+                else {
+                    _super.prototype.init.call(this).done(function () {
+                        if (_this.ctx || _this.get_templateConfiguration().get_preventAutoload()) {
+                            // ---[ELEMENTS-BLOCK-START]---
+                            // ---[ELEMENTS-BLOCK-END]---
+                            dfd.resolve();
+                        }
+                        else {
+                            dfd.resolve();
+                        }
+                    }).fail(dfd.reject);
                 }
             });
         }
@@ -49,17 +64,6 @@ var Template_imageViewer_vizit = (function (_super) {
             dfd.resolve();
         }
         
-        // Wird nicht mehr benötigt, wenn der vizit Viewer alle Viewer ersetzen soll
-        /*_super.prototype.init.call(this).done(function () {
-            if (_this.ctx || _this.get_templateConfiguration().get_preventAutoload()) {
-                // ---[ELEMENTS-BLOCK-START]---
-                // ---[ELEMENTS-BLOCK-END]---
-                dfd.resolve();
-            }
-            else {
-                dfd.resolve();
-            }
-        }).fail(dfd.reject);*/
         
         return dfd.promise();
     };
