ee040534993c3ed84db14609ee41cf4bc5cfe029
Elsbecker Alexander <alexander.elsbecker@d-velop.de>
2016-07-26 11:46:25 +0200

    Update imageViewer_vizit.js

--- Einbindung eines anderen Viewers - Vizit/imageViewer_vizit.js
+++ Einbindung eines anderen Viewers - Vizit/imageViewer_vizit.js
@@ -16,31 +16,31 @@ var Template_imageViewer_vizit = (function (_super) {
         
         if (_this.ctx || _this.get_templateConfiguration().get_preventAutoload()) {
             
-            // Url der anzuzeigenden Datei ermitteln
+            // URL der anzuzeigenden Datei ermitteln
             this.ctx.getServerRelativeFileUrl().then(function(serverRelativeUrl) {
                 
                 try {
-                    // Optionen beinhalten u.a. die Information darüber wo drin der Viewer gerendert werden soll, hier wird der für die Viewer vorgesehene container ermittelt und das HTMLElement übergeben
+                    // Die Optionen beinhalten u.a. die Information darüber, worin der Viewer gerendert werden soll. Hier wird der für die Viewer vorgesehene Container ermittelt und das HTML-Element übergeben
                     var vizitOptions = { renderTo: _this.container.find(".dvContainer")[0], autoShow: true },
-                        // Für die Ermittlung des Elementes benötigt der Vizit Viewer lediglich die absolute Url der Datei
+                        // Für die Ermittlung des Elementes benötigt der Vizit Viewer lediglich die absolute URL der Datei
                         itemObject = { url: ecspand.Core.getServerUrl() + serverRelativeUrl };
                         
-                    // Viewer Erstellen (weitere Informationen findet man unter http://files.vizit.com/public/docs/4.0/symbols/Vizit.Essential.Manager.html)
+                    // Viewer erstellen (für weitere Informationen siehe http://files.vizit.com/public/docs/4.0/symbols/Vizit.Essential.Manager.html)
                     Vizit.Essential.Manager.Create("centerVizit", vizitOptions);
                     // Setzen des Items
                     Vizit.Essential.Manager.Open("centerVizit", itemObject);
                     
-                    // Der Visit Viewer wird standardmäßig mit einem fürs center zu geringem z-index dargestellt
+                    // Der Vizit Viewer wird standardmäßig mit einem für das center zu geringem z-Index dargestellt
                     $(".x-panel").css({ zIndex: 10});
                 
                     dfd.resolve();
                 }
                 catch (err) {
-                    // Tritt beim Laden des Viewers ein Fehler auf, so können Ausführliche Informationen im Browser-Log oder als Notification ausgegeben werden
+                    // Tritt beim Laden des Viewers ein Fehler auf, können ausführliche Informationen im Browser-Log oder als Notification ausgegeben werden
                     dfd.reject(ecspand.Helper.Exception.createAndLogException({
                         instance: this, method: "init", 
-                        notificationMessage: "Vizit Viewer konnte nicht geladen werden. Bitte stellen Sie sicher, dass alle Komponenten installiert wurden und eine gültige Lizenz vorhanden ist.<br/> Weiter Informationen finden Sie im Browser-Log.",
-                        message: "Vizit Viewer konnte nicht geladen werden", error: err
+                        notificationMessage: "Der Vizit Viewer konnte nicht geladen werden. Bitte stellen Sie sicher, dass alle Komponenten installiert wurden und eine gültige Lizenz vorhanden ist.<br/> Weitere Informationen finden Sie im Browser-Log.",
+                        message: "Der Vizit Viewer konnte nicht geladen werden", error: err
                     }, false, true));
                 }
             });
@@ -49,7 +49,7 @@ var Template_imageViewer_vizit = (function (_super) {
             dfd.resolve();
         }
         
-        // Wird nicht mehr benötigt, wenn der vizit Viewer alle Viewer ersetzen soll
+        // Wird nicht mehr benötigt, wenn der Vizit Viewer alle Viewer ersetzen soll
         /*_super.prototype.init.call(this).done(function () {
             if (_this.ctx || _this.get_templateConfiguration().get_preventAutoload()) {
                 // ---[ELEMENTS-BLOCK-START]---
