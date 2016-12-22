1ae262741a6b1ee816fd32e4ca887c8e655c3765
Michael Pawlik <michael.pawlik@d-velop.com>
2016-12-22 14:27:47 +0100

    Beispiel zum Hinzufügen einer Thumbnail column hinzugefügt

---
+++ grid - Thumbnail Column hinzufügen/grid_example.js
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2015 d.velop AG
+ */
+var Template_grid_example = (function (_super) {
+    __extends(Template_grid_example, _super);
+    function Template_grid_example(ctx, folder, options) {
+        _super.call(this, ctx, folder, options);
+        // ---[ELEMENTS-CONFIG-START]---
+        this.extendedConfigSerialized = '{}';
+        // ---[ELEMENTS-CONFIG-END]---
+    }
+
+    Template_grid_example.prototype.init = function () {
+        var _this = this;
+        var dfd = $.Deferred();
+        var self = this;
+        
+        // Weiter mit dem Standard-Vorgehen
+        return _super.prototype.init.call(_this).done(function () {
+            if (_this.ctx || _this.get_templateConfiguration().get_preventAutoload()) {
+                // ---[ELEMENTS-BLOCK-START]---
+                // ---[ELEMENTS-BLOCK-END]---
+                
+                // Aufruf der Logik zum Hinzufügen der neuen column
+                _this.addThumbnail();
+                
+                dfd.resolve();
+            }
+            else {
+                dfd.resolve();
+            }
+        });
+    };
+    
+    Template_grid_example.prototype.addThumbnail = function () {
+        var _this = this;
+        
+        // Temporärer Workaround um ein Timingproblem zu lösen
+        setTimeout(function() {
+
+            var oldColumns = _this.grid.options.columns, // zuvor anhand der aktuellen View intern generierte Columns
+                template = "#= Template_grid_example.formatThumbnail(data,'FileLeafRef') #", // Kendo ruft intern eine angegebene Funktion auf in der die column gerendert werden soll. Diese muss immer statisch sein
+                newColumn = { field: "FileLeafRef", title: "Thumbnail", width: 150, template: template, sortable: false, filterable: false, internalName: "FileLeafRef" };
+                
+            // Icon wird durch das Thumbnail ersetzt
+            oldColumns.splice(0, 1, newColumn);
+                
+            _this.grid.setOptions({ columns: oldColumns });
+            _this.grid.refresh();
+
+            _this.resize();
+        }, 10);
+    };
+    
+    // Statische Funktion zum generieren der Column
+    Template_grid_example.formatThumbnail = function(data, field) {
+
+        var template = '<img src="{0}" style="height: 150px" onload="SP.ScriptHelpers.resizeImageToSquareLength(this, 150)"/>',
+            item = data[field],
+            value = item && item.value ? item.value : "";
+        
+        return template.format("http://mpaw-vs2012-01/Bildbib/_w/" + value.replace(/\./g, "_") + ".jpg");
+        
+    };
+
+    Template_grid_example.prototype.destroy = function () {
+        _super.prototype.destroy.call(this);
+    };
+
+    return Template_grid_example;
+})(Template_grid);
+
+//# sourceURL=grid_example.js
---
+++ grid - Thumbnail Column hinzufügen/readme.md
@@ -0,0 +1,28 @@
+# grid - Wechseln der View #
+
+## Voraussetzungen ##
+
+* ecspand 3.1.0.10
+
+Da das ``grid`` für Anpassungen noch nicht offiziell freigegeben wurde (kein Konfigurator vorhanden), muss 
+zunächst manuell eine Ableitung des Templates mit Hilfe des ``derivation``-Templates erstellt werden. 
+
+Des Weiteren wird das ``grid``-Template automatisch vom ``tabs-Template`` geladen, weshalb es nicht automatisch in der
+FileConfigration eingebunden werden kann. Hierzu muss das neue Template einmal manuell als Definition registriert und in 
+der jeweiligen FileConfiguration hinterlegt werden.
+
+> Zu beachten ist noch, dass das ``grid``-Template immer jeweils aus der Konfiguration
+des angezeigten Kind-Elementes angezogen wird und nicht aus der Konfiguration des aktuell angezeigten Elementes.
+
+> Beispiel: 
+- Kundenakte
+ - Aufträge 
+>
+Ist hier die Kundenakte geöffnet und es soll unter dem Tab "Aufträge" etwas ausgeblendet werden, 
+muss das Template in der FileConfiguration der "Aufträge" Akte eingebunden werden.
+ 
+
+## Bekannte Probleme ##
+> Timout ist nötig, da es sonst zu einer Exception kommt, die anschließend das Paging verhindert
+> Beim wechseln der View verschwindet die Thumbnail column aktuell
+> Url zur Bibliothek muss aktuell noch vorgegeben werden
