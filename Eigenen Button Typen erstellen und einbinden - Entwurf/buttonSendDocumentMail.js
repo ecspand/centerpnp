0a5a4f9bd10eb59ea65da67e7203079f5f6dd69a
D-VELOP\mbey <marvin.beyer@d-velop.de>
2017-02-01 11:08:12 +0100

    Beispiel zum Erstellen für einen eigenen Button Typ weiter implementiert

--- Eigenen Button Typen erstellen und einbinden - Entwurf/buttonSendDocumentMail.js
+++ Eigenen Button Typen erstellen und einbinden - Entwurf/buttonSendDocumentMail.js
@@ -6,64 +6,59 @@ var Element_buttonSendDocumentMail = (function (_super) {
         this.templateID = "elementConfiguratorButtonSendDocumentMail-Template";
         this.hoverPanel = null;
         this.formViewModel = null;
-        
-        this.mailObject = {
-            url: "https://api.sendgrid.com/v3/mail/send/beta",
-            apiKey: "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",
-            apiKeyName: "apikey"        
-        };
-        
-        this.formViewTemplate = 
-        '<div class="mailContainer" style="width: 300px; height: 200px">' +
-            '<h2>Mail versenden</h2>' +
-            '<table style="width: 97%; margin: 10px 0px 10px 0px;">' +
-                '<tr class="formItem display">' +
-                    '<td class="name">' +
-                        '<span class="displayName">Adresse</span>' +
-                        '<div class="description">Mailadresse des Empfängers</div>' +
-                    '</td>' +
-                    '<td class="value">' +
-                        '<input type="text" data-bind="value: email" />' +
-                    '</td>' +
-                '</tr>' +
-            '</table>' +
-            '<a data-role="button" data-bind="enabled: btnSendEnabled, click: btnSendClick">senden</a>' +
-        '</div>';
-        
-        var configTemplate = 
-        '<script id="elementConfiguratorSocialFeed-Template" type="text/x-kendo-template">' +
-            '<div class="templateConfigurator" style="width: 100%; min-width: 600px">' +
 
-                '<div style="min-height: 300px; width: 100%">' +
-                    '<h2 style="margin-top: 8px; margin-left: 20px">#= ecspand.cultures.templateConfigurator.defaultButtonSpecialSettings #</h2>' +
-                    '<table style="width: 97%; margin: 10px 0px 10px 0px;">' +
-                        '#= kendo.render(kendo.template($(\'\\#elementConfiguratorDefaultButtonBaseSettings-Template\').html()),[data]) #' +
+        this.formViewTemplate =
+            '<div class="mailContainer" style="width: 300px; height: 200px">' +
+                '<h2>Mail versenden</h2>' +
+                    '<table style="width: 97%; margin: 10px 0px 10px 0px">' +
                         '<tr class="formItem display">' +
                             '<td class="name">' +
-                                '<span class="displayName">Titel</span>' +
-                                '<div class="description">Titel einer Nachricht. Platzhalter: {dateiname}</div>' +
+                                '<span class="displayName">Absender</span>' +
+                                '<span class="description">Empfänger</span>' +
                             '</td>' +
                             '<td class="value">' +
-                                '<input type="text" data-bind="enabled: enabled, value: messageTitle" />' +
-                            '</td>' +
-                        '</tr>' +
-                        '<tr class="formItem display">' +
-                            '<td class="name">' +
-                                '<span class="displayName">Nachricht</span>' +
-                                '<div class="description">Standard-Nachricht die beim Verschicken einer Datei als Text angehängt wird. Platzhalter: {dateiname}</div>' +
-                            '</td>' +
-                            '<td class="value">' +
-                                '<input type="text" data-bind="enabled: enabled, value: message" />' +
+                                '<span data-bind="text: fromEmail"></span>' +
+                                '<input type="text" placeholder="z.B. name@beispiel.de" data-value-update="keyup" data-bind="value: email" />' +
                             '</td>' +
                         '</tr>' +
                     '</table>' +
+                '<a data-role="button" data-bind="enabled: btnSendEnabled, click: btnSendClick">Senden</a>' +
+            '</div>';
+
+        this.configTemplate =
+            '<script id="elementConfiguratorButtonSendDocumentMail-Template" type="text/x-kendo-template">' +
+                '<div class="templateConfigurator" style="width: 100%; min-width: 600px">' +
+
+                    '<div style="min-height: 300px; width: 100%">' +
+                        '<h2 style="margin-top: 8px; margin-left: 20px">#= ecspand.cultures.templateConfigurator.defaultButtonSpecialSettings #</h2>' +
+                        '<table style="width: 97%; margin: 10px 0px 10px 0px;">' +
+                            '#= kendo.render(kendo.template($(\'\\#elementConfiguratorDefaultButtonBaseSettings-Template\').html()),[data]) #' +
+                            '<tr class="formItem display">' +
+                                '<td class="name">' +
+                                    '<span class="displayName">Titel</span>' +
+                                    '<div class="description">Titel einer Nachricht. Platzhalter: {dateiname}</div>' +
+                                '</td>' +
+                                '<td class="value">' +
+                                    '<input type="text" data-bind="enabled: enabled, value: messageTitle" />' +
+                                '</td>' +
+                            '</tr>' +
+                            '<tr class="formItem display">' +
+                                '<td class="name">' +
+                                    '<span class="displayName">Nachricht</span>' +
+                                    '<div class="description">Standard-Nachricht die beim Verschicken einer Datei als Text angehängt wird. Platzhalter: {dateiname}</div>' +
+                                '</td>' +
+                                '<td class="value">' +
+                                    '<input type="text" data-bind="enabled: enabled, value: message" />' +
+                                '</td>' +
+                            '</tr>' +
+                        '</table>' +
+                    '</div>' +
                 '</div>' +
-            '</div>' +
-        '</script>';
-        $(document.body).append(this.formViewTemplate);
-        
+            '</script>';
+        $(document.body).append(this.configTemplate);
+
     }
-    
+
     // In dieser Methode wird geprüft, ob der Button überhaupt geladen werden soll. So sollte hier z.B. geprüft werden, ob der Benutzer die Aktion die Button bereitstellt überhaupt ausführen darf. In dem Beispiel wird geprüft,
     // ob es sich bei dem Element um ein Dokument handelt
     Element_buttonSendDocumentMail.prototype.isEnabled = function () {
@@ -77,44 +72,78 @@ var Element_buttonSendDocumentMail = (function (_super) {
         }
         return dfd.resolve(isDocument);
     };
-    
+
     // Click-Methode welche automatisch über das ViewModel der Basisklasse aufgerufen wird wenn die Schaltfläche geklickt wird
     Element_buttonSendDocumentMail.prototype.onClick = function (e) {
-        
+        var _this = this;
+
         this.formViewModel = kendo.observable({
-           btnSendClick: this.onBtnSendClick.bind(this),
-           btnSendEnabled: function() {
+            btnSendClick: this.onBtnSendClick.bind(this),
+            btnSendEnabled: function () {
                 var email = this.get("email");
+                var fromEmail = this.get("fromEmail");
+                var isValid = true;
+
+                [fromEmail, email].forEach(function(itemToTest) {
+                    if(itemToTest === null || itemToTest === undefined || itemToTest === "")
+                        isValid = false;
+                });
                 
-                return email !== null && email !== undefined && email !== "";  
-           },
-           email: null
+                return isValid;
+            },
+            email: null,
+            fromEmail: null
         });
-        this.hoverPanel = new ecspand.Controls.HoverPanel($("#" + this.id), { closeOnClick: true });
-        
+
+        this.hoverPanel = new ecspand.Controls.HoverPanel($("#" + _this.id), { closeOnClick: true });
         var template = $(this.formViewTemplate);
         kendo.bind(template, this.formViewModel);
-        
-        this.hoverPanel.setContent(template);
         this.hoverPanel.show();
+
+        this._loadUserEmail().done(function (data) {
+            _this.formViewModel.set("fromEmail", data.userMail);
+            
+            _this.hoverPanel.setContent(template);
+        }).fail(function (error) {
+            SP.UI.Notify.addNotification("<span style='color: red'>Es konnte keine Absender Adresse gefunden werden.</span>");
+        });
     };
-    
-    Element_buttonSendDocumentMail.prototype.onBtnSendClick = function() {
+
+    //In dieser Methode wird die E-Mail Adresse des aktuellen Benutzers asynchron geladen
+    Element_buttonSendDocumentMail.prototype._loadUserEmail = function () {
+        var dfd = $.Deferred();
+        var clientContext = SP.ClientContext.get_current();
+        var user = clientContext.get_web().get_currentUser();
+
+        clientContext.load(user);
+        clientContext.executeQueryAsync(function () {
+        dfd.resolve({
+            userMail: user.get_email()
+        });
+        }, function (error) {
+            dfd.reject(error);
+        });
+
+        return dfd.promise();
+    };
+
+    //Click-Methode, welche über das Form-ViewModel aufgerufen wird, wenn der Senden-Button gedrückt wurde
+    Element_buttonSendDocumentMail.prototype.onBtnSendClick = function () {
         var _this = this;
 
         this.downloadFileData(
             this.template.ctx.get_site().get_url(),
             this.template.ctx.get_webRelativeUrl(),
             this.template.ctx.get_listItem().get_item("FileRef"))
-            .then(function(data) {
+            .then(function (data) {
 
                 var fileName = _this.template.ctx.get_listItem().get_item("FileLeafRef"),
                     fileType = fileName.substr(fileName.lastIndexOf("."), fileName.length - 1),
                     mailData = {
-                        personalizations: [{ "to": [{ "email": "YOU@sendgrid.com" }] }],
-                        from: { "email": "dx@sendgrid.com" },
-                        subject: "Hello, World!",
-                        content: [{ type: "text/plain", value: "Heya!" }],
+                        personalizations: [{ "to": [{ "email": _this.formViewModel.get("email") }] }],
+                        from: { "email": _this.formViewModel.get("fromEmail") },
+                        subject: _this.getDefaultViewModel().get("messageTitle"),
+                        content: [{ type: "text/plain", value: _this.getDefaultViewModel().get("message") }],
                         attachments: [
                             {
                                 content: data,
@@ -124,23 +153,22 @@ var Element_buttonSendDocumentMail = (function (_super) {
                         ]
                     };
 
-
+                //Bei der Methode SendMail handelt es sich um eine eigene Web-Methode zum versenden von E-Mails
                 $.ajax({
-                    url: _this.mailObject.url,
+                    url: "/_api/web/SendMail",
                     headers: {
-                        "Authorization": "Bearer" + _this.mailObject.apiKey,
                         "Content-Type": "application/json"
                     },
                     data: mailData
-                }).done(function() {
+                }).done(function () {
                     SP.UI.Notify.addNotification("Datei wurde erfolgreich per Mail versandt.");
-                }).fail(function(a, b, c) {
-                    SP.UI.Notify.addNotification("<span style='color: red>Beim Verschicken der Mail kam es zu einem Problem.");
+                }).fail(function (a, b, c) {
+                    SP.UI.Notify.addNotification("<span style='color: red'>Beim Verschicken der Mail kam es zu einem Problem.</span>");
                 });
             });
     };
-    
-    Element_buttonSendDocumentMail.prototype.downloadFileData = function(siteUrl, webRelativeUrl, fileServerRelativeUrl) {
+
+    Element_buttonSendDocumentMail.prototype.downloadFileData = function (siteUrl, webRelativeUrl, fileServerRelativeUrl) {
         var _this = this;
         var dfd = $.Deferred();
 
@@ -152,37 +180,37 @@ var Element_buttonSendDocumentMail = (function (_super) {
             type: "GET",
             dataType: "arraybuffer",
             accepts: "*/*",
-            success: function(data) {
+            success: function (data) {
                 dfd.resolve(data);
             }
         };
 
-        $.ajax(settings).fail(function(jqXHR, textStatus, errorThrown) {
+        $.ajax(settings).fail(function (jqXHR, textStatus, errorThrown) {
             dfd.reject(new ecspand.Data.Exception({
                 class: _this, method: "downloadFileData",
                 message: "Could not download file: " + errorThrown, objects: [jqXHR]
             }));
         });
-        
+
         return dfd.promise();
     };
-       
+
     // Diese Methode gibt alle Informationen aus dem zuvor erstellen ViewModel zurück, die für eine Speicherung relevant sind
     Element_buttonSendDocumentMail.prototype.getConfigSettings = function () {
         // Konfiguration der Basisklasse abrufen
         var copy = _super.prototype.getConfigSettings.call(this);
-        
+
         // und anschließend bereinigen oder ergänzen - es ist grundsätzlich nur nötig die in dieser Klasse neu hinzugefügten Eigenschaften zu bereinigen
         // Eigenschaften die auf "undefined" gesetzt werden, werden gar nicht erst serialisiert
-        
-        copy.additionalSelectProperties = undefined;
-        
+
+        copy.dataSource = undefined;
+
         return copy;
     };
-    
+
     // Diese Mthode gibt das Standard-ViewModel zurück, welches hauptsächlich für die Darstellung und konfigurierbare Funktionalität des eigentlichen Buttons benötigt wird
     Element_buttonSendDocumentMail.prototype.getDefaultViewModel = function () {
-        
+
         var vm = {
             defaultTitle: function () { // Standard-Titel des Buttons - kann in der Konfiguration manuell angepasst werden
                 return "Mein toller Button";
@@ -197,19 +225,19 @@ var Element_buttonSendDocumentMail = (function (_super) {
             showInFurtherOptions: false,// Soll standardmäßig nicht unter "weitere Optionen" angezeigt werden
             showInFileCoverMenu: true, // Soll standardmäßig im normalen Menü angezeigt werden
             //showOnlyInFileCoverMenu: true, // Soll ausschließlich im normalen Menü angezeigt werden
-            message: null,
-            messageTitle: null
+            message: "Meine Nachricht",
+            messageTitle: "Mein Titel"
         };
-        
+
         return kendo.observable($.extend(true, _super.prototype.getDefaultViewModel.call(this), vm));
     };
-    
+
     Element_buttonSendDocumentMail.prototype.getEditViewModel = function () {
-        
+
         var vm = {
-            
+            //dataSource: new kendo.data.DataSource({})
         };
-       
+
         return _super.prototype.getEditViewModel.call(this, vm);
     };
     return Element_buttonSendDocumentMail;
