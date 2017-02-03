e6f03ae482d861cc389cb319e0ef95535b8ed7ad
D-VELOP\mbey <marvin.beyer@d-velop.de>
2017-02-03 14:32:20 +0100

    Gulp watch hinzugefügt. Dateien werden nun nach .src kompiliert.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 
-var paths = [
-    "src/*.ts",
-    "Eigenen Tab Typen erstellen und einbinden/*.ts"
-]
-
 gulp.task("default", function () {
-    return gulp.src(paths, { base: '.' })
+    return tsProject.src()
         .pipe(tsProject())
-        .js.pipe(gulp.dest('.'));
+        .js.pipe(gulp.dest('.src'));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch("./**/*.ts", ['default']);
 });
